86014facec6c6c95484c297dd006d13fc7a8ec13
Jose Quijada <jose.quijada@showtime.net>
2020-03-26 14:32:52 -0400

    feat(groovy): latest

--- src/main/java/com/exsoinn/Closure.groovy
+++ src/main/java/com/exsoinn/Closure.groovy
@@ -1,10 +1,32 @@
-def str = [269754.0,"PHFOY","SONIF LAB MIA","USA","MARRIOTT"].each {
-  String retStr = pStr.replaceAll("(\\d+)\\.0", "\$1")
+/*def str = [269754.0,"PHFOY","SONIF LAB MIA","USA","MARRIOTT"].each {
+  String retStr = it.replaceAll("(\\d+)\\.0", "\$1")
     return retStr
 }.join("|")
 
-println str
+println str*/
 
 def obj1 = new Expando(a: 1, b: 2)
 def obj2 = new Expando(a: 1, b: 2)
-def someList = [], 
+def someList = []
+
+
+// Variables delacred in closure local scope
+def listOfMapsOne = [['a': 1], ['b': 2]]
+def listOfMapsTwo = [['c': 3], ['d': 4]]
+
+def cumulativeMap = [:]
+def clearedMap = [:]
+listOfMapsOne.each { Map one -> 
+  listOfMapsTwo.each { Map two ->
+    clearedMap.clear()
+    cumulativeMap << one << two
+    clearedMap << one << two
+    def localMap = [:]
+    localMap << one << two
+    println "localMap is ${localMap}"
+  }
+}
+
+println cumulativeMap
+println clearedMap
+

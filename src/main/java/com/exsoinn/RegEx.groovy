fa101789bebccb39620ee6a6f71ea608c62a9695
Jose Quijada <jose.quijada@showtime.net>
2020-04-09 19:47:07 -0400

    feat(groovy): latest

--- src/main/java/com/exsoinn/RegEx.groovy
+++ src/main/java/com/exsoinn/RegEx.groovy
@@ -11,3 +11,19 @@ Matcher m = patt.matcher(str)
 
 m.find()*/
 //println m.group()
+
+
+/*
+ * Inspect what a matcher returns w/o groupings versus w/ groupings
+ */
+ def matcherWithoutGroupings = 'a:1 b:2 c:3' =~ /\S:\S/
+
+matcherWithoutGroupings.each {
+  println "Match w/o groupings: $it"
+}
+
+def matcherWithGroupings = 'a:1 b:2 c:3' =~ /(\S):(\S)/
+
+matcherWithGroupings.each {
+  println "Match with groupings: $it"
+}

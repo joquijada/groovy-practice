2a7c2f4593dfc0e02cdddfa6179a4f006abea15d
Jose Quijada <jose.quijada@showtime.net>
2020-04-09 20:29:35 -0400

    feat(groovy): latest

--- src/main/java/com/exsoinn/Binary.groovy
+++ src/main/java/com/exsoinn/Binary.groovy
@@ -1,10 +1,19 @@
-def propertyMissing(String name) {
+def propertyMissing2(String name) {
   int result = 0
   name.each {
     result <<= 1
+    //println "" // throws StackOverFlow exception because the property missing hook is invoked recursively
     if (it == 'I') result++
   }
   return result
 }
 
-assert
+//println IIOI + IOI
+
+/*
+ * Does the JVM use 2'a complement?
+ */
+def five = 5
+def negFive = -5
+println "$five in binary is ${Integer.toBinaryString(five)}" 
+println "$negFive in binary is ${Integer.toBinaryString(negFive)}" 

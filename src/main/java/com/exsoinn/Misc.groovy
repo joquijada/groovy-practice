94d2fc9a76893856a7df5f94f9cba5f0b515733b
Jose Quijada <jose.quijada@showtime.net>
2020-12-05 00:27:35 -0500

    feat(groovy): latest

--- src/main/java/com/exsoinn/Misc.groovy
+++ src/main/java/com/exsoinn/Misc.groovy
@@ -4,3 +4,11 @@
 //println empty.sum()?.size() ?: 0
 //String[] ary = ['one', 'two']
 //println "${ary.toList()}"
+
+def list1 = ['a', 'b', 'c']
+def list2 =['a', 'b', 'c', 'd']
+
+println list1 - list2
+println list2 - list1
+
+print list1 + 'e'

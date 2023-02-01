02e44b8311c729ca3142bb283a44ed7600d8a385
Jose Quijada <joquijada2010@gmail.com>
2023-02-01 15:49:16 -0500

    feat(groovy): latest

--- src/main/java/com/exsoinn/MathFun.groovy
+++ src/main/java/com/exsoinn/MathFun.groovy
@@ -1,3 +1,9 @@
-def num = 7543
-assert num.intdiv(num) == 1
-assert num.intdiv(num + 1) == 0
+byte partitionSize = Byte.SIZE
+println partitionSize
+long totalPages = (Integer.MAX_VALUE+1L)*2/partitionSize
+println "totalPages is $totalPages ($totalPages.class.name)"
+println ((long)(Integer.MAX_VALUE+1)*2/partitionSize)
+
+//def num = 7543
+//assert num.intdiv(num) == 1
+//assert num.intdiv(num + 1) == 0

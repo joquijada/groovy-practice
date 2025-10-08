c2ad8d28e2a60f71cba53d404243b3c34e122ec5
Jose Quijada <joquijada2010@gmail.com>
2025-10-08 12:48:53 -0400

    save

--- src/main/groovy/com/exsoinn/PropertyBasedTesting.groovy
+++ src/main/groovy/com/exsoinn/PropertyBasedTesting.groovy
@@ -1,6 +1,5 @@
 package com.exsoinn
 
-@Grab('net.java.quickcheck:quickcheck:0.6')
 import static net.java.quickcheck.generator.PrimitiveGenerators.*
 import static java.lang.Math.round
 import static Converter.celsius
@@ -13,4 +12,10 @@ def liquidF = 32..212
   int c = round(celsius(f))
   assert c <= f
   assert c in liquidC == f in liquidF
+}
+
+class Converter {
+  static float celsius(int farenheit) {
+    (farenheit-32)*5/9
+  }
 }
